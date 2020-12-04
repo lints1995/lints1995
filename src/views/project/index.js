63dd6a4aa078bccd2e0ef913322e2630df755c53
lints <LinTianSheng@newhope.cn>
2020-12-04 16:38:06 +0800

    添加导航

---
+++ src/views/project/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+
+function Project() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <h1>项目</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
