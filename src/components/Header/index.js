7bb4a7c614eedf0e920da347ea9cf23eb45fab1f
lints <LinTianSheng@newhope.cn>
2020-12-09 17:07:13 +0800

    添加页面

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import HEADER_MENU from "../../constant/headerMenu";
-import "./index.scss";
+import styles from "./index.scss";
 
 class Header extends React.Component {
   render() {
     return (
-      <header className="header">
+      <header className={styles.header}>
         <ul className="items">
           {HEADER_MENU.map((item) => {
             return (
--- src/views/resume/index.js
+++ src/views/resume/index.js
@@ -1,11 +1,29 @@
 import Header from "../../components/Header";
+import PROJECT_LISTS from "../../constant/projectLists";
+import "./index.scss";
+
+function Items(item) {
+  return (
+    <div className="items" key={item.id}>
+      <h1 className="item-title">{item.name}</h1>
+      <h2 className="item-sub-title">项目描述：</h2>
+      <p>{item.discription}</p>
+      <h2 className="item-sub-title">职责：</h2>
+      <div dangerouslySetInnerHTML={{ __html: item.content }}></div>
+      <h2 className="item-sub-title">总结：</h2>
+      <p>{item.summary}</p>
+    </div>
+  );
+}
 
 function Project() {
   return (
     <div>
       <Header />
       <div className="container">
-        <h1>简历</h1>
+        {PROJECT_LISTS.map((item) => {
+          return Items(item);
+        })}
       </div>
     </div>
   );
