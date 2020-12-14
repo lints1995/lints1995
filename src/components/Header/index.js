390008239bca94391765b55221b2a085666114b0
lints <LinTianSheng@newhope.cn>
2020-12-14 14:31:02 +0800

    添加导航样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,14 @@ import HEADER_MENU from "../../constant/headerMenu";
 import styles from "./index.module.scss";
 
 class Header extends React.Component {
+  componentDidMount() {
+    console.log(this.props.location.pathname);
+  }
+  judgeCurrentActivePath = (path) => {
+    // 获取当前路径
+    let pathname = this.props.location.pathname;
+    return pathname === path ? styles["item-active"] : null;
+  };
   render() {
     return (
       <header className={styles.header}>
@@ -11,12 +19,23 @@ class Header extends React.Component {
           {HEADER_MENU.map((item) => {
             return (
               <li className={styles.item} key={item.id}>
-                {item.path.includes("http") ? (
-                  <a href={item.path} target="_blank" rel="noreferrer">
+                {item.path.includes("http") || item.path.includes("https") ? (
+                  <a
+                    className={this.judgeCurrentActivePath(item.path)}
+                    href={item.path}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     {item.name}
                   </a>
                 ) : (
-                  <Link className={styles.link} to={item.path}>
+                  <Link
+                    className={[
+                      styles.link,
+                      this.judgeCurrentActivePath(item.path),
+                    ].join(" ")}
+                    to={item.path}
+                  >
                     {item.name}
                   </Link>
                 )}
