a762589b84e6faaf3219f18cd57502aadb017660
lints <LinTianSheng@newhope.cn>
2020-12-17 15:05:19 +0800

    修改路由模式为hash模式

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./views/home";
 import Resume from "./views/resume";
