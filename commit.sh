0917436ffb20be7f2832f8f55b9a1192c7ca09ba
lints1995 <lints1995@163.com>
2021-05-22 14:23:35 +0800

    更新脚

--- commit.sh
+++ commit.sh
@@ -1,14 +1,17 @@
 # !/bin/zsh
 # 确保脚本抛出遇到的错误
-# set -e
-# # 生成文章目录
-# node generateArticleCatalog.js
-# # 打包部署静态文件
-# npm run build
-# git pull origin master
+set -e
+# 生成文章目录
+node generateArticleCatalog.js
+# 打包部署静态文件
+npm run build
+
+echo -e "\033[32m 输入提交到的远程分支：\033[0m"
+read -p "" branch
+git pull origin $branch
 # 提交
 git add -A
-read -p "输入提交到的远程分支:" branch
-read -p "输入提交到的远程分支:" description
+echo -e "\033[32m 输入commit提交信息：\033[0m"
+read -p  "" description
 git commit -m $description
 git push origin $branch
