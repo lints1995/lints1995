525fb539d78f1c8f685145d068c8bef1832909a2
lints1995 <lints1995@163.com>
2021-05-22 14:11:51 +0800

    更新脚本文件

--- commit.sh
+++ commit.sh
@@ -5,14 +5,10 @@
 # node generateArticleCatalog.js
 # # 打包部署静态文件
 # npm run build
-# echo "输入要提交的分支"
-echo $barch
-echo -n "输入提交到的远程分支名："
-read
-echo "提交到远程分支： $REPLY"
-git pull origin $REPLY
+# git pull origin master
 # 提交
 git add -A
-echo $description
+read -p "输入提交到的远程分支:" branch
+read -p "输入提交到的远程分支:" description
 git commit -m $description
-git push origin $REPLY
+git push origin $branch
