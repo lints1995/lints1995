43da8ad924083a69d60c057d0ac203e3e2223eb5
lints1995 <lints1995@163.com>
2021-05-22 14:05:13 +0800

    优化页面样式

--- commit.sh
+++ commit.sh
@@ -1,11 +1,18 @@
 # !/bin/zsh
 # 确保脚本抛出遇到的错误
-set -e
-# 生成文章目录
-node generateArticleCatalog.js
-# 打包部署静态文件
-npm run build
+# set -e
+# # 生成文章目录
+# node generateArticleCatalog.js
+# # 打包部署静态文件
+# npm run build
+# echo "输入要提交的分支"
+echo $barch
+echo -n "输入提交到的远程分支名："
+read
+echo "提交到远程分支： $REPLY"
+git pull origin $REPLY
 # 提交
 git add -A
-git commit -m '内容更新'
-git push origin master
+echo $description
+git commit -m $description
+git push origin $REPLY
