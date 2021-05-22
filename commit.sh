e3a907ceef6e856f03e264b59f9a5a696a075883
lints1995 <lints1995@163.com>
2021-05-22 14:31:14 +0800

    优化脚本

--- commit.sh
+++ commit.sh
@@ -6,12 +6,12 @@ node generateArticleCatalog.js
 # 打包部署静态文件
 npm run build
 
-echo -e "\033[32m 输入提交到的远程分支：\033[0m"
+echo -e "\033[32m 输入提交到的远程分支 \033[0m"
 read -p "输入完成敲击enter：" branch
 git pull origin $branch
 # 提交
 git add -A
-echo -e "\033[32m 输入commit提交信息：\033[0m"
+echo -e "\033[32m 输入commit提交信息 \033[0m"
 read -p "输入完成敲击enter：" description
 git commit -m $description
 git push origin $branch
